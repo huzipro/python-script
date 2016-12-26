e0efea54f83750ce605d5bea91aa2bcd89d45f89
AffanIndo <AffanIndo@users.noreply.github.com>
2016-12-26 12:16:28 +0700

    add numberGuess.py

---
+++ pythonScript/numberGuess.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python
+"""
+numberGuess, by AffanIndo, 7-11-16.
+Guess the number.
+"""
+
+import random
+
+def main():
+    number = random.randint(1,20) #decide the number to be guessed
+    print('NUMBER GUESS')
+    loop = True
+    while loop == True:
+        answer = int(input('guess a number between 1-20: '))
+        if answer > number:
+            print('Too high')
+        elif answer < number:
+            print('Too low')
+        elif answer == number:
+            print('You are right!, the answer is %s' % (number))
+            loop = False
+        else:
+            print('please guess a number between 1-20')
+
+    playAgain = input('Do you want to play again? (y/n)')
+    if playAgain == 'y':
+        main()
+    else:
+        exit()
+    
+
+if __name__ == '__main__':
+    main()
