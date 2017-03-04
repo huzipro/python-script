85337a76cd1fd743c6001a7443d838b5e2323775
AffanIndo <AffanIndo@users.noreply.github.com>
2017-03-04 21:42:29 +0700

    error

--- pythonScript/fileBracketRemover.py
+++ pythonScript/fileBracketRemover.py
@@ -1,14 +1,8 @@
-
-"""
-fileBracketRemover.py
-Mass remove "()" (brackets) from file name in whole folder
-file (1), file (2) => file 1, file 2
-"""
-
 import os
 import glob
 files = glob.glob('*(*).txt')
 for file in files:
     new_name = file.replace("(", "")
     new_name2 = new_name.replace(")", "")
+    print(new_name2)
     os.rename(file, new_name2)
