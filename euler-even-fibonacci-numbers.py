1869cc37ed914ceb807ceab091dffba5aa56f9e9
AffanIndo <AffanIndo@users.noreply.github.com>
2017-03-10 19:04:40 +0700

    fix euler fib

--- euler-even-fibonacci-numbers.py
+++ euler-even-fibonacci-numbers.py
@@ -1,3 +1,10 @@
+#!usr/bin/env python
+"""
+euler-even-fibonacci-numbers.py
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+"""
+
+
 fibList = []
 
 a, b = 0, 1
