b70facd062c0f6eff77ec7fce796a2f2085dd0f6
AffanIndo <AffanIndo@users.noreply.github.com>
2017-03-31 19:15:56 +0700

    added euler factorial digit sum

--- euler-largest-palindrome-product.py
+++ euler-largest-palindrome-product.py
@@ -4,6 +4,7 @@ euler-largest-palindrome-product.py
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+
 palindrome = []
 for x in range(100, 1000):
   for y in range(100, 1000):
