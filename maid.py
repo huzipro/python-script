5d4b0605f75607cd9481849d4dd2be24e551bcdc
AffanIndo <AffanIndo@users.noreply.github.com>
2017-04-01 11:27:05 +0700

    edited maid.py

--- maid.py
+++ maid.py
@@ -1,9 +1,14 @@
 import time
 import winsound
 
+initS = winsound.PlaySound('./maid_files/init.wav', winsound.SND_FILENAME)
+blipS = winsound.PlaySound('./maid_files/blip.wav', winsound.SND_FILENAME);
+
+def backup:
+  pass
+
 def main():
-  winsound.PlaySound('./maid_files/init.wav', winsound.SND_FILENAME);
-  
+  initS
 
 if __name__ == '__main__':
   main()
