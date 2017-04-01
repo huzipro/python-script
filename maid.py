1c6b8a4994301b98fb73d53b98e3a57a658a1ab7
AffanIndo <AffanIndo@users.noreply.github.com>
2017-04-01 12:09:49 +0700

    fisx maid.py

--- maid.py
+++ maid.py
@@ -1,14 +1,40 @@
-import time
+
+"""
+maid.py
+My personal assistant
+"""
+
 import winsound
 
-initS = winsound.PlaySound('./maid_files/init.wav', winsound.SND_FILENAME)
-blipS = winsound.PlaySound('./maid_files/blip.wav', winsound.SND_FILENAME);
+# sound
+def initSound(): winsound.PlaySound('./maid_files/init.wav', winsound.SND_FILENAME)
+def blipSound(): winsound.PlaySound('./maid_files/blip.wav', winsound.SND_FILENAME)
 
-def backup:
-  pass
+def help():
+  print("""
+The thing that I understand:
+help
+backup
+""")
 
+# features
+def backup():
+  print('backuped')
+  blipSound()
+
+# start the program
 def main():
-  initS
+  initSound()
+  while True:
+    command = input('\nWhat will be your pleasure today sir?\n>>> ').split(' ')[0]
+    if command == 'help':
+      help()
+    elif command == 'backup':
+      backup()
+    else:
+      print('I don\'t understand that command sir')
+      main()
+    print('Understood.')
 
 if __name__ == '__main__':
   main()
