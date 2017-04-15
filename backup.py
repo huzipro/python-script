5f3d5b96235993a948d12f305b4f4724de6bac2c
AffanIndo <AffanIndo@users.noreply.github.com>
2017-04-15 21:04:18 +0700

    edited backup

--- backup.py
+++ backup.py
@@ -1,3 +1,9 @@
+
+"""
+backup.py
+This is the script to backup my files to ./backupFiles/ directory. I already put the backup folder inside .gitignore so it will not uploaded to the repository. If the files already exists. It will replace the files so it will update the backup files.
+"""
+
 import shutil
 
 # Important files in ~
@@ -9,11 +15,27 @@ dotFiles = ['C:\\Users\\AFFAN\\.bash_history',
             'C:\\Users\\AFFAN\\.minttyrc',
             'C:\\Users\\AFFAN\\.vimrc',]
 
+textFiles = ['D:\\E-BOOK\\Text\\000webhost.jpg',
+             'D:\\E-BOOK\\Text\\000webhost.txt',
+             'D:\\E-BOOK\\Text\\Ammar Advice.txt',
+             'D:\\E-BOOK\\Text\\Design.txt',
+             'D:\\E-BOOK\\Text\\Dream.jpg',
+             'D:\\E-BOOK\\Text\\FFmpeg.txt',
+             'D:\\E-BOOK\\Text\\Gimp Tweak.txt',
+             'D:\\E-BOOK\\Text\\Gmail.txt',
+             'D:\\E-BOOK\\Text\\Linux Command.txt',
+             'D:\\E-BOOK\\Text\\Linux Summary.txt',
+             'D:\\E-BOOK\\Text\\Misc Account.txt',
+             'D:\\E-BOOK\\Text\\Path.txt',
+             'D:\\E-BOOK\\Text\\Universitas.txt',
+             ]
+
 for file in dotFiles:
-    try:
-        # Create the folder first
-        # The folder is not relative path because I use this as an alias
-        # shutil.copy2(file, './backupFiles/dotFiles/')
-        shutil.copy2(file, 'D:\\PROGRAMMING\\git\\python-script\\backupFiles\\dotFiles')
-    except PermissionError:
-        print("Error occured: " + PermissionError)
+    # The folder is not relative path because I use this as an alias
+    # shutil.copy2(file, './backupFiles/dotFiles/')
+    shutil.copy2(file, 'D:\\PROGRAMMING\\git\\python-script\\backupFiles\\dotFiles')
+
+for file in textFiles:
+    shutil.copy2(file, 'D:\\PROGRAMMING\\git\\python-script\\backupFiles\\textFiles')
+
+print('\n  Backup success! no error occured.')
